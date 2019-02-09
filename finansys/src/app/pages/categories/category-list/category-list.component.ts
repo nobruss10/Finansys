2f465ce751bc5da3935123c1ad03be953ee000b3
Bruno santos <bruno@sequenza.com.br>
2019-02-09 20:37:38 -0200

    improving category list component

--- finansys/src/app/pages/categories/category-list/category-list.component.ts
+++ finansys/src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../shared/category.model';
+import { CategoryService } from '../shared/category.service';
+import { element } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) { }
+
+  categories: Category[] = []
 
   ngOnInit() {
+    this.categoryService.getAll()
+      .subscribe(
+        categories => this.categories = categories,
+        error => alert("Erro ao carregar a lista")
+      )
+  }
+
+  deleteCategory(category) {
+    const mostDelete = confirm("Deseja realmente excluir esse item?")
+
+    if (mostDelete)
+      this.categoryService.delete(category)
+        .subscribe(
+          () => this.categories = this.categories.filter(element => element != category),
+          () => alert("Erro ao tentar excluir")
+        )
   }
 
 }
