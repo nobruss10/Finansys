ba8eea53b94e9967b6009f676ff74a6426e0cead
Bruno santos <bruno@sequenza.com.br>
2019-02-09 08:57:42 -0200

    Adding category model

--- finansys/src/app/in-memory-database.ts
+++ finansys/src/app/in-memory-database.ts
@@ -1,14 +1,16 @@
 
 import { InMemoryDbService } from "angular-in-memory-web-api";
 
+import { Category } from './pages/shared/category.model';
+
 export class InMemoryDataBase implements InMemoryDbService {
     createDb() {
-        const categories = [
+        const categories: Category[] = [
             {id: 1, name: 'Moradia', description: 'Pagamento de contas da casa'},
             {id: 1, name: 'Saúde', description: 'Plano de Saúde e Remédios'},
             {id: 1, name: 'Lazer', description: 'Cinema, parques, praia e etc'},
             {id: 1, name: 'Salário', description: 'Recebimento de salário'},
-            {id: 1, name: 'Freelas', description: 'Trabalho como freelance'}
+            {id: 1, name: 'Freelas', description: 'Trabalho como freelanc'}
         ]
 
         return { categories }
